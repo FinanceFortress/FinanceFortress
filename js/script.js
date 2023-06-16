fa9baf35b116bb3ca27b7e7b113cb40d0b91f339
dimassssk <105980822+dimassssk@users.noreply.github.com>
2023-06-16 21:58:29 +0300

    the end

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+    // Navigation arrows
+     slidesPerView: 1, //how mach?
+       spaceBetween: 10,
+    navigation: {
+        // my btn
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      // bullets
+      clickable: true,
+      dynamicBullets: true,
+    },
+     keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+     watchOverflow: true,
+  
+  });
