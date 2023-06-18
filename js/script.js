854359657444fea4fa742bcc5d6614a15d200c43
dimassssk <105980822+dimassssk@users.noreply.github.com>
2023-06-18 16:49:38 +0300

    verify

--- js/script.js
+++ js/script.js
@@ -20,3 +20,5 @@ const swiper = new Swiper('.swiper', {
      watchOverflow: true,
   
   });
+
+  (function(i,m,p,a,c,t){c.ire_o=p;c[p]=c[p]function(){(c[p].a=c[p].a[]).push(arguments)};t=a.createElement(m);var z=a.getElementsByTagName(m)[0];t.async=1;t.src=i;z.parentNode.insertBefore(t,z)})('https://utt.impactcdn.com/P-A4396990-9d73-4897-97f6-b8da65f882181.js','script','impactStat',document,window);impactStat('transformLinks');impactStat('trackImpression');
